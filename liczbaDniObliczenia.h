fb9ace88a9e39fadfd7add32c77248b6feafb713
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2022-10-17 19:44:51 +0000

    Wyłączenie definicji metod poza definicję klasy.

--- liczbaDniObliczenia.h
+++ liczbaDniObliczenia.h
@@ -5,35 +5,45 @@ class liczbaDniObliczenia {
         int liczbaDniWmies;
         int liczbaDniWroku;
         
-    void wykonajObliczenia(void) {
-            czyPrzestępny();
-            ustawLiczbęDniWRoku();
-            ustawLiczbęDniWMiesiącu();
-        }
+    void wykonajObliczenia(void);
     
     private:
         bool czyRokPrzestępny;
-        void czyPrzestępny(void) {
-            this -> czyRokPrzestępny = ((this -> rok % 4 == 0 && this -> rok % 100 != 0) || 
-                                         this -> rok % 400 == 0);
-        }   
-    
-        void ustawLiczbęDniWRoku(void) { this -> liczbaDniWroku =  czyRokPrzestępny ? 366 : 365; }
-    
-        void ustawLiczbęDniWMiesiącu(void) {
-            switch (this -> miesiąc) {
-              case 1:
-              case 3:
-              case 5:
-              case 7:
-              case 8:
-              case 10:
-              case 12:
-                  this -> liczbaDniWmies =  31;
-              case 2:
-                  this -> liczbaDniWmies = czyRokPrzestępny ? 29 : 28;
-              default:
-                  this -> liczbaDniWmies = 30;
-            }  
-        }
+        void czyPrzestępny(void);
+        void ustawLiczbęDniWRoku(void);
+        void ustawLiczbęDniWMiesiącu(void);     
 };
+
+
+void liczbaDniObliczenia::wykonajObliczenia(void) {
+    czyPrzestępny();
+    ustawLiczbęDniWRoku();
+    ustawLiczbęDniWMiesiącu();
+}
+
+
+void liczbaDniObliczenia::czyPrzestępny(void) {
+    this -> czyRokPrzestępny = ((this -> rok % 4 == 0 && this -> rok % 100 != 0) || 
+                                 this -> rok % 400 == 0);
+}
+
+
+void liczbaDniObliczenia::ustawLiczbęDniWRoku(void) { this -> liczbaDniWroku =  czyRokPrzestępny ? 366 : 365; }
+
+
+void liczbaDniObliczenia::ustawLiczbęDniWMiesiącu(void) {
+    switch (this -> miesiąc) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            this -> liczbaDniWmies =  31;
+        case 2:
+            this -> liczbaDniWmies = czyRokPrzestępny ? 29 : 28;
+        default:
+            this -> liczbaDniWmies = 30;
+    }  
+}
