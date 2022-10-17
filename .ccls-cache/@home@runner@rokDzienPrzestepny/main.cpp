da16a37acab63b11347c4db55d4ca610547d5d9d
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2022-10-17 18:00:08 +0000

    Osobne klasy

--- .ccls-cache/@home@runner@rokDzienPrzestepny/main.cpp
+++ .ccls-cache/@home@runner@rokDzienPrzestepny/main.cpp
@@ -1,51 +1,78 @@
 #include <iostream>
 using namespace std;
-int miesiąc, rok;
+
 /*
 Program oblicza liczbę dni w miesiącu i liczbę dni w roku dla podanego miesiąca
 i roku. Użycie wypiszLiczbeDniNaMiesiącRok(int miesiąc, int rok). Np.
 wypiszLiczbeDniNaMiesiącRok(2, 2020).
 */
 
-bool czyPrzestępny(int rok) {
-  return ((rok % 4 == 0 && rok % 100 != 0) || rok % 400 == 0);
-}
+class InterfejsUżytkownika {
+    public: 
+        void static pobierzDane(int &miesiąc, int &rok) {
+            cout << "Podaj miesiąc i rok odzielone spacją > ";
+            cin >> miesiąc >> rok; 
+        }
+    
+        void static wypiszLiczbeDniNaMiesiącRok(const int miesiąc, 
+                                         const int rok,
+                                         const int liczbaDniWmies, 
+                                         const int liczbaDniWroku) {
+            cout << "W " << miesiąc << ". miesiącu roku " << rok << " jest "
+                 << liczbaDniWmies << " dni." << endl;
+            
+            cout << "Rok " << rok << " ma " << liczbaDniWroku << " dni." << endl << endl;
+        }
+};
 
-int dajLiczbęDniWRoku(int rok) { return czyPrzestępny(rok) ? 366 : 365; }
+class LiczbaDniObliczenia {
+    public: 
+        int miesiąc;
+        int rok;
+        int liczbaDniWmies;
+        int liczbaDniWroku;
+        
+    void wykonajObliczenia(void) {
+            czyPrzestępny();
+            ustawLiczbęDniWRoku();
+            ustawLiczbęDniWMiesiącu();
+        }
+    
+    private:
+        bool czyRokPrzestępny;
+        void czyPrzestępny(void) {
+            this -> czyRokPrzestępny = ((this -> rok % 4 == 0 && this -> rok % 100 != 0) || 
+                                         this -> rok % 400 == 0);
+        }   
+    
+        void ustawLiczbęDniWRoku(void) { this -> liczbaDniWroku =  czyRokPrzestępny ? 366 : 365; }
+    
+        void ustawLiczbęDniWMiesiącu(void) {
+            switch (this -> miesiąc) {
+              case 1:
+              case 3:
+              case 5:
+              case 7:
+              case 8:
+              case 10:
+              case 12:
+                  this -> liczbaDniWmies =  31;
+              case 2:
+                  this -> liczbaDniWmies = czyRokPrzestępny ? 29 : 28;
+              default:
+                  this -> liczbaDniWmies = 30;
+            }  
+        }
+};
 
-int dajLiczbęDniWMiesiącu(int miesiąc, bool czyPrzestępny) {
-  switch (miesiąc) {
-      case 1:
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-          return 31;
-      case 2:
-          return czyPrzestępny ? 29 : 28;
-      default:
-          return 30;
-  }
-}
-
-void wypiszLiczbeDniNaMiesiącRok(int miesiąc, int rok) {
-  bool przestępny = czyPrzestępny(rok);
-  cout << "W " << miesiąc << ". miesiącu roku " << rok << " jest "
-       << dajLiczbęDniWMiesiącu(miesiąc, przestępny) << " dni." << endl;
-  cout << "Rok " << rok << " ma " << dajLiczbęDniWRoku(rok) << " dni." << endl
-       << endl;
-}
-
-void pobierzDane(void) {
-    cout << "Podaj miesiąc i rok odzielone spacją > ";
-    cin >> miesiąc >> rok; 
-}
 
 int main() {
-
-  pobierzDane();
-  wypiszLiczbeDniNaMiesiącRok(miesiąc, rok);
-  main();
+    LiczbaDniObliczenia liczbaDniObl;
+    InterfejsUżytkownika::pobierzDane(liczbaDniObl.miesiąc, liczbaDniObl.rok);
+    liczbaDniObl.wykonajObliczenia();
+    InterfejsUżytkownika::wypiszLiczbeDniNaMiesiącRok(liczbaDniObl.miesiąc,
+                                                      liczbaDniObl.rok, 
+                                                      liczbaDniObl.liczbaDniWmies, 
+                                                      liczbaDniObl.liczbaDniWroku);
+    main();
 }
